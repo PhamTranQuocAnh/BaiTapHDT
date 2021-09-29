f08f1f765ae85d47db57c58ac5fed13e8d0d8e46
PhamTranQuocAnh <quocanhpham262@gmail.com>
2021-09-29 22:28:08 +0700

    initial commit

--- src/com/anhptq2008110037/lab3/Phan1Bai1.java
+++ src/com/anhptq2008110037/lab3/Phan1Bai1.java
@@ -1,12 +1,26 @@
+/*
+*  crated date: 29 thg 9, 2021
+*  author: PhamTranQuocAnh
+*/
 package com.anhptq2008110037.lab3;
-import java.until.scanner;
-public class Phan1Bai {
-    public static void main(String[] args){
-        boolean ok = true ;
-        scanner bienNhap = new scanner(System.in);
-         System.out.println("Nhập vào 1 số nguyên: ");
-         int N=bienNhap.nextInt();
-         for (int i = 2; i<N-1;i++)
 
+import java.util.Scanner;
+
+public class Phan1Bai1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Nhập vào số a để kiểm tra có phải là số nguyên tố: ");
+        int a = in.nextInt();
+        boolean ok = true;
+        for (int i = 2;i<a;i++)
+            if(a%i==0)
+            {
+                ok = false;
+                break;
+            }
+        if(ok==true)
+            System.out.println("Đây là số nguyên tố ");
+        else
+            System.out.println("Đây không là số nguyên tố ");
     }
 }
