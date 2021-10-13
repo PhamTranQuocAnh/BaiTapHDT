50f9a3afdc9cb1223bc55e5740de9d63e7a93aa5
PhamTranQuocAnh <quocanhpham262@gmail.com>
2021-10-13 22:24:05 +0700

    initial commic

---
+++ src/com/anhptq2008110037/lab5/TaiKhoan.java
@@ -0,0 +1,30 @@
+/*
+*  crated date: 13 thg 10, 2021
+*  author: PhamTranQuocAnh
+*/
+package com.anhptq2008110037.lab5;
+
+public class TaiKhoan {
+    String soDu;
+    String soTaiKhoan;
+    String tenTaiKhoan;
+    String diaChi;
+    String tenNganHang;
+    String namSinh;
+    String ngayCapThe;
+    TaiKhoan() {
+        tenNganHang = "ViettinBank";
+    }
+    TaiKhoan(String ten) {
+        tenTaiKhoan = ten;
+    }
+    TaiKhoan(String nS, String dc) {
+        namSinh = nS;
+        diaChi = dc;
+    }
+    TaiKhoan(String stk, String sD, String ngayCap) {
+        soTaiKhoan = stk;
+        soDu = sD;
+        ngayCapThe = ngayCap;
+    }
+}
---
+++ src/com/anhptq2008110037/lab5/TaiKhoanTestDrive.java
@@ -0,0 +1,30 @@
+/*
+*  crated date: 13 thg 10, 2021
+*  author: PhamTranQuocAnh
+*/
+package com.anhptq2008110037.lab5;
+
+public class TaiKhoanTestDrive {
+    public static void main(String[] args) {
+        TaiKhoan tk = new TaiKhoan();
+        System.out.println("Tên ngân hàng: " + tk.tenNganHang);
+        System.out.println();
+
+        TaiKhoan tk1 = new TaiKhoan("David Four");
+        System.out.println("Tên khách hàng: " + tk1.tenTaiKhoan);
+        System.out.println();
+
+        TaiKhoan tk2 = new TaiKhoan("01/12/2002", "518/32 - Đồng Tháp");
+        System.out.println("Ngày Sinh: " + tk2.namSinh);
+        System.out.println("Địa chỉ: " + tk2.diaChi);
+        System.out.println();
+
+        TaiKhoan tk3 = new TaiKhoan("123546789", "10.000 VNĐ", "06/06/2016");
+        System.out.println("Số tài khoản: " + tk3.soTaiKhoan);
+        System.out.println("Số dư: " + tk3.soDu);
+        System.out.println("Ngày cấp thẻ: " + tk3.ngayCapThe);
+        System.out.println();
+
+    }
+
+}
