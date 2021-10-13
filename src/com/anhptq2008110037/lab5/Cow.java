f1f2aac7e13cf5891aa56dc291537fecd2051719
PhamTranQuocAnh <quocanhpham262@gmail.com>
2021-10-14 01:52:57 +0700

    initial commic

---
+++ src/com/anhptq2008110037/lab5/Cow.java
@@ -0,0 +1,34 @@
+/*
+*  crated date: 14 thg 10, 2021
+*  author: PhamTranQuocAnh
+*/
+package com.anhptq2008110037.lab5;
+public class Cow {
+    // attribute
+    double weight;
+    double age;
+    Cow (){
+        weight = 15;
+        age = 1;
+    }
+    // hàm tạo 1 tham số
+    Cow (double w){
+        weight = w;
+        age = 2;
+    }
+    // hàm tạo 2 tham số
+    Cow (double w, double a){
+        weight = w;
+        age = a;
+    }
+    void inThongTin (){
+        System.out.print("Cân Nặng :" + weight);
+        System.out.println(" kg");
+        System.out.print("Tuổi: "+age);
+        System.out.println(" tuổi");
+    }
+    void moo(){
+        System.out.println("Be be be");
+    }
+    
+}
---
+++ src/com/anhptq2008110037/lab5/CowTestDrive.java
@@ -0,0 +1,22 @@
+/*
+*  crated date: 14 thg 10, 2021
+*  author: PhamTranQuocAnh
+*/
+package com.anhptq2008110037.lab5;
+
+public class CowTestDrive {
+    public static void main (String[] args){
+        Cow cow;
+        cow = new Cow();
+        cow.inThongTin();
+        cow.moo();
+        cow = new Cow (10);
+        cow.inThongTin();
+        cow.moo();
+        cow = new Cow(9, 1);
+        cow.inThongTin();
+        cow.moo();
+
+    }
+    
+}
