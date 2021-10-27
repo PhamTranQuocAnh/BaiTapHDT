5d6384540fabe1c2a84297ffc178bef672f3df1c
PhamTranQuocAnh <quocanhpham262@gmail.com>
2021-10-27 22:50:52 +0700

    initial commic

---
+++ src/com/anhptq2008110037/Inheritance/Shape/Amoeba.java
@@ -0,0 +1,30 @@
+/*
+*  crated date: 27 thg 10, 2021
+*  author: PhamTranQuocAnh
+*/
+package com.anhptq2008110037.Inheritance.Shape;
+
+public class Amoeba extends Shape {
+
+    private String nameFile;
+    private int toaDoX;
+    private int toaDoY;
+
+    Amoeba(String n, int x, int y) {
+        nameFile = n;
+        toaDoX = x;
+        toaDoY = y;
+    }
+
+    protected void hinhDang() {
+        System.out.println("Đây là một hình kiểu Amoeba");
+    }
+
+    protected void xoay() {
+        System.out.printf("Hình quay theo tọa độ x = " + toaDoX + " và y = " + toaDoY + "\n");
+    }
+
+    protected void soud() {
+        System.out.println("Mở file âm thanh: " + nameFile);
+    }
+}
---
+++ src/com/anhptq2008110037/Inheritance/Shape/Square.java
@@ -0,0 +1,11 @@
+/*
+*  crated date: 27 thg 10, 2021
+*  author: PhamTranQuocAnh
+*/
+package com.anhptq2008110037.Inheritance.Shape;
+
+public class Square extends Shape {
+    protected void hinhDang() {
+        System.out.println("Đây là hình vuông");
+    }
+}
