3a9119b4a0ad8992215efe5a99850bbe1d1af724
PhamTranQuocAnh <quocanhpham262@gmail.com>
2021-10-27 22:41:51 +0700

    initial commic

---
+++ src/Tuan7/Inheritance/Shape/Circle.java
@@ -0,0 +1,11 @@
+/*
+*  crated date: 27 thg 10, 2021
+*  author: PhamTranQuocAnh
+*/
+package Tuan7.Inheritance.Shape;
+
+    public class Circle extends Shape {
+        protected void hinhDang() {
+            System.out.println("Đây là hình tròn");
+        }
+    }
